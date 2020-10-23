aabe78211bafea902b0299ce6ebbb1ef7f2b5ae5
Suvighya Dubey <suvighyad@gmail.com>
2020-10-23 12:47:46 +0530

    broken our homepage iinto smaller components, finished styling our mennu-item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div className='App'>
-      <h1>HELLO FOR COMMIT</h1>
       <HomePage />
     </div>
   );
